7786468ffbf4dba2f07670ac4c921f5e6fafaac2
spac3y <73653851+Spac3y@users.noreply.github.com>
2023-04-02 14:55:33 +0300

    update html

--- Donate_Code/javascript.js
+++ Donate_Code/javascript.js
@@ -8,9 +8,12 @@ function getCurentDate() {
     var yyyy = today.getFullYear();
 
     today = dd + '/' + mm + '/' + yyyy;
+    return today;
 }
 
 function dateDifference() {
-    splitDateList = endDate.split('/');
-    console.log(splitDateList);
+    splitDateEndList = endDate.split('/');
+    splitDateCurrentList = getCurentDate().split('/');
+    console.log(splitDateCurrentList);
+    daysLeft = (splitDateEndList[2]-splitDateCurrentList[2])*365 + (splitDateEndList[1]-splitDateCurrentList[1])*31 + splitDateEndList[0] - splitDateCurrentList[0]
 }
